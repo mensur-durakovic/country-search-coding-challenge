0e470d5b0c02e7b5bd897616793c44efd8b822e7
Mensur Durakovic <mensur.durakovic@pseudocode.agency>
2020-05-02 09:50:03 +0200

    added helper function to calculate distance and client part

---
+++ utils/utilies.js
@@ -0,0 +1,24 @@
+
+/**
+ * calculates distance between 2 geo-points using Haversine formula
+ * @param {number} lat1 - latitude of first geo-point
+ * @param {number} lat2 - latitude of second geo-point
+ * @param {number} lon1 - longitude of first geo-point
+ * @param {number} lon2 - longitude of second geo-point
+ */
+function calculateDistance(lat1, lat2, lon1, lon2) {
+  const p = Math.PI / 180;
+  const result =
+    0.5 -
+    Math.cos((lat2 - lat1) * p) / 2 +
+    (Math.cos(lat1 * p) *
+      Math.cos(lat2 * p) *
+      (1 - Math.cos((lon2 - lon1) * p))) /
+      2;
+
+  return 12742 * Math.asin(Math.sqrt(result)); // 2 * R; R = 6371 km
+}
+
+module.exports = {
+    calculateDistance: calculateDistance
+};
